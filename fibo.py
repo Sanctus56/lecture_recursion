bb548f17dfc9a286f7050353b869877645039514
Sanctus56 <alex.kostolansky32@gmail.com>
2024-04-22 12:13:05 +0200

    Fibonacci

--- fibo.py
+++ fibo.py
@@ -1,7 +1,18 @@
 
+def recursive_nth_fibo(n):
+    if n == 0:
+        return n
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
+
 def main():
     pass
 
 
 if __name__ == "__main__":
+    print(recursive_nth_fibo(9))
     main()
